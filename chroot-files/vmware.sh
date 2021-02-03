0e8fbd3d4034b57bb8ef4d85c39e999fde905731
David Kwasny <dkwasny67@gmail.com>
2021-02-02 22:02:13 -0500

    Update vmware.sh
    Updating what packages and services are setup for VMware guest setup.

--- chroot-files/vmware.sh
+++ chroot-files/vmware.sh
@@ -3,7 +3,11 @@
 # Additional tasks to get the Arch running in VMWare
 
 echo "Installing VMWare packages."
-pacman -S xf86-video-vmware open-vm-tools;
+pacman -S \
+	xf86-video-vmware \
+	xf86-input-vmmouse \
+	open-vm-tools;
 
 echo "Enabling VMWare services."
-systemctl enable vmware-vmblock-fuse;
+systemctl enable vmtoolsd;
+#systemctl enable vmware-vmblock-fuse;
