92edbf2d4d753b12aa59857c4ac0990f66a2e017
David Kwasny <dkwasny67@gmail.com>
2021-03-26 03:33:16 -0400

    Update vmware.sh

--- chroot-files/vmware.sh
+++ chroot-files/vmware.sh
@@ -2,12 +2,16 @@
 
 # Additional tasks to get the Arch running in VMWare
 
-echo "Installing VMWare packages."
+echo "Installing VMWare packages.";
 pacman -S \
 	xf86-video-vmware \
 	xf86-input-vmmouse \
 	open-vm-tools;
 
-echo "Enabling VMWare services."
+echo "Enabling VMWare services.";
 systemctl enable vmtoolsd;
 #systemctl enable vmware-vmblock-fuse;
+
+# See https://bbs.archlinux.org/viewtopic.php?id=223470
+echo "Fixing VM scroll behavior";
+echo "GDK_CORE_DEVICE_EVENTS=1" >> /etc/environment;
