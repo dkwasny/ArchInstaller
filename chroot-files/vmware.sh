dc00fe65e4ae042bbd792849437aaa593aed7056
David Kwasny <dkwasny67@gmail.com>
2021-03-26 14:18:37 -0400

    Update vmware.sh

--- chroot-files/vmware.sh
+++ chroot-files/vmware.sh
@@ -2,6 +2,12 @@
 
 # Additional tasks to get the Arch running in VMWare
 
+# Add the following lines to the VMX file to make scrolling not feel like hot garbage.
+# Auto mouse (un)capture is lost, and the cursor is laggy, but its better than broken scrolling.
+#
+# mks.gamingMouse.policy = "gaming"
+# mouse.vusb.useBasicMouse=FALSE
+
 echo "Installing VMWare packages.";
 pacman -S \
 	xf86-video-vmware \
@@ -11,7 +17,3 @@ pacman -S \
 echo "Enabling VMWare services.";
 systemctl enable vmtoolsd;
 #systemctl enable vmware-vmblock-fuse;
-
-# See https://bbs.archlinux.org/viewtopic.php?id=223470
-echo "Fixing VM scroll behavior";
-echo "GDK_CORE_DEVICE_EVENTS=1" >> /etc/environment;
